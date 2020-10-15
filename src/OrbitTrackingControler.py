d1e4a241cd013bdcee1558ad5f3fd145ea5337d9
ryoryo666 <sakaryo0102@gmail.com>
2020-10-14 23:33:24 -0700

    school

--- src/OrbitTrackingControler.py
+++ src/OrbitTrackingControler.py
@@ -2,14 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
-def Pub():
-    x_r=rospy.get_param("reference_x")
-    y_r=rospy.get_param("reference_y")
-    vx_r=
-    vy_r=
+def New_cmd(odom):
+	x_cmd=
+	y_cmd=
+	v_cmd=
+
+def Set():
+	rospy.init_node("Kanayama_Method_Controller", anonymous=True)
+	rospy.Subscriber("/robot_gazebo/diff_drive_controller/odom", Odometry, New_cmd)
+	pub=rospy.Publisher("/robot_gazebo/diff_drive_controller/cmd_vel", Twist, queue_size=2)
+
+	rospy.spin()
+
 
 if __name__=="__main__":
     try:
-        Pub()
+    	Set()
     except rospy.ROSInterruptException: pass
